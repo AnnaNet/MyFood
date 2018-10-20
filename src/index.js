502b196afd3c128096563963e04f725b6b310234
Anna Moskalchuk <anna@crystax.net>
2018-10-20 13:29:31 +0300

    fix: size images, *.scss

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-/*import $ from 'jquery';*/
 import 'bootstrap';
 
 $(document).ready(() => {
   console.log ('Hello!');
-
-  $('.tmp').css({'background-color': 'blue'});
-  $('.tmp').css('background-color', 'green');
 });
